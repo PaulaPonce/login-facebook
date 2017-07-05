42b71a84e84bafbcbe3020067b94c82c615338e4
Paula <paula.poncez@usach.cl>
2017-07-05 12:27:45 -0400

    facebook login button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+//Facebok SDK
 window.fbAsyncInit = function() {
   FB.init({
     appId : '314153372362694',
@@ -13,11 +13,19 @@ window.fbAsyncInit = function() {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {return;}
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9&appId=314153372362694";
   fjs.parentNode.insertBefore(js, fjs);
 }
 (document, 'script', 'facebook-jssdk'));
 
+//Check login status
 FB.getLoginStatus(function(response) {
   statusChangeCallback(response);
 });
+
+//Login button
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
